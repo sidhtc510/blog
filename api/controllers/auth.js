2e1e63ae2d8ed09975bad85ef7368070833385c2
sidhtc510 <sidhtc510@gmail.com>
2023-04-10 13:14:40 +0200

    login & register

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -32,6 +32,7 @@ export const login = (req, res) => {
   const q = "SELECT * FROM users WHERE email = ?";
 
   db.query(q, [req.body.email], (err, data) => {
+
     if (err) return res.json(err);
     if (data.length === 0) return res.status(404).json("user not found");
 
@@ -44,6 +45,11 @@ export const login = (req, res) => {
       return res.status(400).json("Wrong username or password");
 
       const token = jwt.sign({id:data[0].id}, "jwtkey")
+      const {password, ...other } = data[0]
+
+      res.cookie("access_token", token, {
+        httpOnly: true
+      }).status(200).json(other)
   });
 };
 
