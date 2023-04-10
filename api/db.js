baaf942425704fbcd30e9de0c9e2c3da0c47822c
sidhtc510 <sidhtc510@gmail.com>
2023-04-10 13:48:31 +0200

    .

--- api/db.js
+++ api/db.js
@@ -4,6 +4,6 @@ export const db = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-    password: "20121984",
+    password: "12345678",
     database: "blog",
 })
