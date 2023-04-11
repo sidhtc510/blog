e831eed355bbd6a56d4f698a2d56fc231edcd0c5
sidhtc510 <sidhtc510@gmail.com>
2023-04-11 19:56:22 +0200

    auth & registration functionality is complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from "../context/authContext";
 
 export default function Login() {
   let [inputs, setInputs] = useState({
@@ -11,16 +12,22 @@ export default function Login() {
   const [err, setError] = useState(null);
 
   const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+  // console.log(currenUser);
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    inputs.email = inputs.email.trim()
+    inputs.email = inputs.email.trim();
 
     try {
-      await axios.post("/auth/login", inputs);
+      // await axios.post("/auth/login", inputs);
+      await login(inputs)
       navigate("/");
     } catch (err) {
       setError(err.response.data);
