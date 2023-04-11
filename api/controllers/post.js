d99217af500e9018ff8901c6ca13820aafb62f81
sidhtc510 <sidhtc510@gmail.com>
2023-04-11 22:48:39 +0200

    delete post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,54 @@
-export const addPost = (req,res)=>{
-    res.json("from post controller")
-}
+import { db } from "../db.js";
+import jwt from "jsonwebtoken";
+
+export const getPosts = (req, res) => {
+  // res.json("from post controller")
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).json(data);
+  });
+};
+
+export const getPost = (req, res) => {
+  const q = "SELECT * FROM users u JOIN posts p ON u.id=p.uid WHERE p.id=?";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data[0]);
+  });
+};
+
+export const addPost = (req, res) => {
+  res.json("from post controller");
+};
+
+export const deletePost = (req, res) => {
+  const token = req.cookies.access_token;
+
+  if (!token) return res.status(401).json("Not authentificated");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+
+
+    if (err) return res.status(403).json("Token is not valid");
+
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE id = ? AND uid =?";
+
+    db.query(q, [postId, userInfo.id], (err,data)=>{
+        
+        if(err) return res.status(403).json("You can delete only your post")
+
+        return res.json("Post has been deleted")
+    });
+  });
+};
+
+export const updatePost = (req, res) => {
+  res.json("from post controller");
+};
