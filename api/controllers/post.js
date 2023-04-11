fee5265488d211177e98e7cffed0acb90e9de497
sidhtc510 <sidhtc510@gmail.com>
2023-04-12 01:03:06 +0200

    add update with bugs. will fix tomorow

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -24,7 +24,29 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  res.json("from post controller");
+  console.log(req);
+  const token = req.cookies.access_token;
+
+  if (!token) return res.status(401).json("Not authentificated");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    const q =
+      "INSERT INTO posts (`title`, `desc`, `img`, `cat`, `date`, `uid`) VALUES (?)";
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      req.body.date,
+      userInfo.id,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("post has been created");
+    });
+  });
 };
 
 export const deletePost = (req, res) => {
@@ -33,22 +55,33 @@ export const deletePost = (req, res) => {
   if (!token) return res.status(401).json("Not authentificated");
 
   jwt.verify(token, "jwtkey", (err, userInfo) => {
-
-
     if (err) return res.status(403).json("Token is not valid");
 
     const postId = req.params.id;
     const q = "DELETE FROM posts WHERE id = ? AND uid =?";
 
-    db.query(q, [postId, userInfo.id], (err,data)=>{
-        
-        if(err) return res.status(403).json("You can delete only your post")
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can delete only your post");
 
-        return res.json("Post has been deleted")
+      return res.json("Post has been deleted");
     });
   });
 };
 
 export const updatePost = (req, res) => {
-  res.json("from post controller");
+  const token = req.cookies.access_token;
+
+  if (!token) return res.status(401).json("Not authentificated");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid");
+    const postId = req.params.id;
+    const q =
+      "UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=? WHERE `id` =? AND `uid` = ?";
+    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("post has been updated");
+    });
+  });
 };
