a1610b7f1719a878bb098901a60c93f9ab184188
sidhtc510 <sidhtc510@gmail.com>
2023-04-11 23:39:21 +0200

    add menu and some styles

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -4,6 +4,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
+import Menu from "../components/Menu";
 
 export default function Single({}) {
   // const params = useParams();
@@ -30,16 +31,14 @@ export default function Single({}) {
     fetchData();
   }, [postId]);
 
-const handleDelete = async ()=>{
-  try {
-    await axios.delete(`/posts/${postId}`);
-    navigate("/")
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     // <div>
@@ -49,21 +48,25 @@ const handleDelete = async ()=>{
     //  <NavLink to={``}>Delete post</NavLink>
     // </div>
 
-    <div>
-      <div>id: {post.id}</div>
-      <div>title: {post.title}</div>
-      <div>description: {post.desc}</div>
-      <div>image: {post?.img}</div>
-      <div>date: {moment(post.date).fromNow()}</div>
-      <div>username: {post.username}</div>
-      <div>category:{post.cat}</div>
+    <div className="singleWrapper">
+      <div className="singlePost">
+        <div>id: {post.id}</div>
+        <div>title: {post.title}</div>
+        <div>description: {post.desc}</div>
+        <div>image: {post?.img}</div>
+        <div>date: {moment(post.date).fromNow()}</div>
+        <div>username: {post.username}</div>
+        <div>category:{post.cat}</div>
 
-      {currentUser && currentUser.username === post.username ? (
-        <div>
-          <NavLink to={`/write/?edit=${post.id}`}>Edit post</NavLink>
-          <NavLink onClick={handleDelete}>Delete post</NavLink>
-        </div>
-      ): null}
+        {currentUser && currentUser.username === post.username ? (
+          <div>
+            <NavLink to={`/write/?edit=${post.id}`}>Edit post</NavLink>
+            <NavLink onClick={handleDelete}>Delete post</NavLink>
+          </div>
+        ) : null}
+      </div>
+   
+      <Menu cat={post.cat} />
     </div>
   );
 }
