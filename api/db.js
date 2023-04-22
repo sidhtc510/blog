cadb74c920750107c6cdf7d58def8c96fa8276cb
sidhtc510 <sidhtc510@gmail.com>
2023-04-23 01:14:44 +0200

    db.js changed

--- api/db.js
+++ api/db.js
@@ -4,6 +4,8 @@ export const db = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
-    password: "12345678",
+    password: "",
     database: "blog",
 })
+
+console.log(db);
