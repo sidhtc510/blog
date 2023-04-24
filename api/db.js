496e94a17c6db6e54bd8ba02730949a80e146a17
sidhtc510 <sidhtc510@gmail.com>
2023-04-24 18:02:53 +0200

    db.js

--- api/db.js
+++ api/db.js
@@ -1,12 +1,12 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    // host: "127.0.0.1",
-    host: "localhost",
+    host: "127.0.0.1",
+    // host: "localhost",
     port: 3306,
     user: "root",
     password: "12345678",
-    database: "blog",
+    database: "blog"
 })
 
 console.log(db);
